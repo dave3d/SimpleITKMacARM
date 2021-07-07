eecc1c4c616387224bc1e6dbb7c2557498047e03
Dave Chen <dchen@mail.nih.gov>
2021-07-07 16:14:09 -0400

    initial checkin

---
+++ build_all.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+Build_Root=$HOME
+SimpleITK_Source=$Build_Root/SimpleITK
+COREBINARYDIRECTORY=$Build_Root/SimpleITK-build
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $Build_Root
+
+# Get the SimpleITK Source
+if [ -d $SimpleITK_Source ]
+then
+    pushd $SimpleITK_Source
+    git pull
+    popd
+else
+    git clone https://github.com/SimpleITK/SimpleITK.git
+fi
+
+mkdir -p $COREBINARYDIRECTORY
+pushd $COREBINARYDIRECTORY
+sh $SCRIPT_DIR/mac_build_core.sh
+popd
+
+mkdir -p py38
+pushd py38
+COREBINARYDIRECTORY=$COREBINARYDIRECTORY sh $SCRIPT_DIR/mac_build_py.sh 3.8
+popd
+
+mkdir -p py39
+pushd py39
+COREBINARYDIRECTORY=$COREBINARYDIRECTORY sh $SCRIPT_DIR/mac_build_py.sh 3.9
+popd
---
+++ mac_build_core.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+# This script will build the C++ code of SimpleITK
+# It should be run in the build directory, e.g. ~/SimpleITK-build
+
+if [ -z "$SimpleITK_Source" ]
+then
+    echo "No source"
+    SimpleITK_Source="~/SimpleITK"
+fi
+echo $SimpleITK_Source
+
+cmake \
+    -D "CMAKE_BUILD_TYPE:STRING=MinSizeRel" \
+    -D "CMAKE_OSX_DEPLOYMENT_TARGET=11.0" \
+    -d "MAKE_OSX_SYSROOT:PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk" \
+    -D "BUILD_EXAMPLES:BOOL=OFF" \
+    -D "BUILD_TESTING:BOOL=ON" \
+    -D "BUILD_SHARED_LIBS:BOOL=OFF" \
+    -D "BUILD_DOCUMENTS:BOOL=OFF" \
+    -D "WRAP_DEFAULT:BOOL=OFF" \
+    -D "WRAP_R:BOOL=OFF" \
+    -D "WRAP_PYTHON:BOOL=OFF" \
+    $SimpleITK_Source/SuperBuild
+
+cmake --build . -- -j4
+
